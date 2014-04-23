2068aa0f1540cb882f79f099f8b7629e02fb53ee
Joe Bauser <coderjoe@coderjoe.net>
2014-04-22 23:18:58 -0400

    berks init

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# 0.1.0
+
+Initial release of ircd-hybrid
+
+* Enhancements
+  * an enhancement
+
+* Bug Fixes
+  * a bug fix
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'ircd-hybrid'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures ircd-hybrid'
+long_description 'Installs/Configures ircd-hybrid'
+version          '0.1.0'
+
