b2b8369e787375de115c7710e8d3ec8b90d0b41e
Joe Bauser <coderjoe@coderjoe.net>
2014-04-26 20:29:05 -0400

    Remove incorrect copyright notice in default recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,10 +1,2 @@
 # Encoding: utf-8
-#
-# Cookbook Name:: ircd-hybrid
-# Recipe:: default
-#
-# Copyright (C) 2014 Joe Bauser <coderjoe at coderjoe.net>
-#
-# All rights reserved - Do Not Redistribute
-#
 include_recipe 'ircd-hybrid::source'
